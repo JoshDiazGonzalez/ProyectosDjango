96d0bbe568180b744278f5b8aed328822c052c53
JoshDiazGonzalez <josethp1@hotmail.com>
2024-02-09 22:15:16 -0500

    add pages of core

--- 01-BlogPersonal/core/views.py
+++ 01-BlogPersonal/core/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, "core/home.html")
+    return render(request, "home.html")
 
 
 def about(request):
-    return render(request, "core/about.html")
+    return render(request, "about.html")
 
 
 def contact(request):
-    return render(request, "core/contact.html")
+    return render(request, "contact.html")
