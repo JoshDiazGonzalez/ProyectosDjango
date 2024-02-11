a09aa8f806e7c8a13071f74ac77077bdca1e36c6
JoshDiazGonzalez <josethp1@hotmail.com>
2024-02-10 19:14:52 -0500

    create app social

--- 02-WebEmpresarial/webempresa/urls.py
+++ 02-WebEmpresarial/webempresa/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     # Paths del core
@@ -24,4 +26,7 @@ urlpatterns = [
     path('services/', include('services.urls')),
     # Paths del admin
     path('admin/', admin.site.urls),
-]
+    # Paths de blog
+    path('blog/', include('blog.urls')),
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #muestra las imagenes
